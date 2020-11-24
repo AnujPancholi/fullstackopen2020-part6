9797a8c32afce892547089b00187de6e240eb6bc
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-11-24 12:49:47 +0530

    feat: SUBMISSION - 6.5 - order anecdotes in descending order of votes

--- app2-redux-anecdotes/src/components/AnecdoteContainer.js
+++ app2-redux-anecdotes/src/components/AnecdoteContainer.js
@@ -2,11 +2,18 @@ import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import Anecdote from './Anecdote'
 import {getUpvoteAction} from "../reducers/anecdoteReducer.js"
+import _ from "lodash"
 
 
 const AnecdoteContainer = () => {
 
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        const copiedState = _.map(state,(obj) => {
+            return _.defaultsDeep({},obj);
+        })
+        copiedState.sort((a,b) => b.votes - a.votes);
+        return copiedState;
+    })
     const dispatch = useDispatch()
 
     const vote = (id) => {
