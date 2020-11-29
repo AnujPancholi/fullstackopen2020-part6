4d44d9ab9908725496b237af692ae66a8ef45e50
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-11-29 11:44:52 +0530

    feat: add create function in anecdotes service to add new anecdote at backend

--- app2-redux-anecdotes/src/services/anecdotes.js
+++ app2-redux-anecdotes/src/services/anecdotes.js
@@ -17,7 +17,41 @@ const getAll = () => {
             return resolve(allAnecdotesAxiosResult.data);
 
         }catch(e){
-            console.log(`service|anecdotes|ERROR`,e);
+            console.log(`service|anecdotes|getAll|ERROR`,e);
+            if(e.response){
+                return reject({
+                    message: "SERVER RESPONDED WITH ERROR",
+                    data: e.response.data
+                })
+            } else if (e.request){
+                return reject({
+                    message: "NO RESPONSE FROM SERVER"
+                })
+            } else {
+                return reject({
+                    message: "AN ERROR OCCURRED IN COMMUNICATING WITH THE SERVER",
+                    detailedMessage: e.message || "UNKNOWN"
+                })
+            }
+        }
+    })
+}
+
+const create = (data) => {
+    return new Promise(async(resolve,reject) => {
+
+        try{
+            data.votes = data.votes || 0;
+            const anecdoteCreationAxiosResult = await anecdotesAxios({
+                method: "POST",
+                url: "/",
+                data: data
+            })
+
+            return resolve(anecdoteCreationAxiosResult.data)
+
+        }catch(e){
+            console.log(`service|anecdotes|create|ERROR`,e);
             if(e.response){
                 return reject({
                     message: "SERVER RESPONDED WITH ERROR",
@@ -39,5 +73,6 @@ const getAll = () => {
 
 
 export default {
-    getAll
+    getAll,
+    create
 }
